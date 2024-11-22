b66582a20705610889387506e111d3e9640c9159
Kullike Annuk <kullike.annuk@voco.ee>
2024-11-22 11:00:06 +0200

    comonent, values, props

--- src/App.js
+++ src/App.js
@@ -2,10 +2,22 @@ import './App.css';
 import ExpenseItem from './components/ExpenseItem';
 
 const App = () => {
+  const expenses = [
+    {
+    date: new Date(2024, 10, 12),
+    title: 'New book',
+    price: 30.99
+    },
+    {
+      date: new Date(2024, 10, 12),
+      title: 'New jeans',
+      price: 99.99
+    }
+   ]
   return (
     <div className="App">
-      <ExpenseItem />
-      <ExpenseItem />
+      <ExpenseItem data={expenses [0]}/>
+      <ExpenseItem data={expenses [1]}/>
     </div>
   );
 }
