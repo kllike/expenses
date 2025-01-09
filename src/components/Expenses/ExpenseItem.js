d3100a61ec11f55b2c943b729dbfc23c58264330
Kullike Annuk <kullike.annuk@voco.ee>
2025-01-09 09:25:14 +0200

    click me test

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,8 +1,16 @@
 import './ExpenseItem.css'
 import ExpenseDate from './ExpenseDate'
 import Card from '../UI/Card' 
+import { useState } from 'react'
 
 const ExpenseItem = (props) => {
+    const [title, setTitle] = useState(props.data.title)
+
+    const clickHandler =() => {
+        console.log('Clicked!')
+        setTitle(`Updated by click ${title}`)
+        console.log(title)
+    }
     return(
         <Card className="expense-item">
             <ExpenseDate date={props.data.date} />
@@ -10,6 +18,7 @@ const ExpenseItem = (props) => {
                 <h2>{props.data.title}</h2>
                 <div className='expense-item__price'>{props.data.price}</div>
             </div>
+            <button onClick={clickHandler}>Click Me</button>  
         </Card>
     )
 } 
